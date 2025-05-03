95ce1b74e708bf7c7371c49b2cf4eaffd4b94253
Kaustav Basu <kaustavb7@gmail.com>
2025-05-04 01:21:50 +0530

    v1.0.0 of the MCP server app for Fintech App

---
+++ src/test/java/com/connectingdots/FintechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectingdots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
